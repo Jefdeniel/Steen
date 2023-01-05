96593c72ad8c2d4ecab483b39c8e9ffc10abc7c4
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2023-01-06 00:07:05 +0100

    create first components

--- Steen_app/src/App.jsx
+++ Steen_app/src/App.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import styles from "./style";
 
-const App = () => {
-  return (
-    <div>
-      <h1>App</h1>
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Foot,
+} from "./components";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>Navbar</div>
     </div>
-  );
-};
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`style.boxWidth`}>Hero</div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`style.boxWidth`}>
+        Stats Business Billing Carddeal testimonials clients CTA Footer
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
---
+++ Steen_app/src/components/Billing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const billing = () => {
+  return <div></div>;
+};
+
+export default billing;
